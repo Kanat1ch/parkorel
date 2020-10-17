bd0ad5a35f27d9185d64fe003e0af25228c2a446
Kanat1ch <ilyakanatnikov@mail.ru>
2020-10-17 21:55:46 +0300

    weather redesigned

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,7 +1,14 @@
 fetch('https://api.openweathermap.org/data/2.5/weather?id=515012&appid=cd0d813f0d0ae69bfc0fbd8a7383c81b&lang=ru')
     .then(function (resp) { return resp.json(); })
     .then(function (data) {
-        document.querySelector('.weather-deg').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
+        let tempInC = Math.round(data.main.temp - 273);
+        if (tempInC > 0) {
+            document.querySelector('.weather-deg').innerHTML = '<small>+</small>' + tempInC + '&deg;';
+        } else if (tempInC < 0) {
+            document.querySelector('.weather-deg').innerHTML = '<small>-</small>' + tempInC + '&deg;';
+        } else {
+            document.querySelector('.weather-deg').innerHTML = tempInC + '&deg;';
+        }
         document.querySelector('.weather-description').textContent = data.weather[0].description;
         document.querySelector('.weather-icon').innerHTML = 
         `<img src="img/weather/${data.weather[0].icon}.png"</img>`;
