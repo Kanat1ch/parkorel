ffa6ac8a879f13181dea8cde74521c943b640f73
Kanat1ch <ilyakanatnikov@mail.ru>
2020-10-18 01:05:15 +0300

    overflow hidden when preloader is active

--- scripts/preloader.js
+++ scripts/preloader.js
@@ -5,11 +5,13 @@ if (sessionStorage.getItem('visited') != '1') {
 }
 function showPreloader() {
     preloader.style.display = 'flex';
+    document.body.style.overflow = 'hidden';
     document.body.onload = function() {
         setTimeout(function() {
             if (!preloader.classList.contains('done')) {
                 preloader.classList.add('done');
             }
+            document.body.style.overflow = '';
         }, 1800);
     };
 }
